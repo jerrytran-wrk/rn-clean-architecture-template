1c1afff60a6d72ea874bccf05b1ac9ca0cc0947c
Jerry Tran <jerrytran.wrk@gmail.com>
2020-09-09 20:16:59 +0700

    CodeGen: Update asset icon generator script to be able multiple folder generating

--- template/script/asset-icon-generator.script.js
+++ template/script/asset-icon-generator.script.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
-const [folder] = process.argv.slice(2);
-const dir = `${__dirname}/${folder}`;
+const path = require('path');
+const dirname = path.dirname(__dirname);
+const folders = process.argv.slice(2);
 
 function replaceAll(raw, regex, replace) {
   while (raw.includes(regex)) {
@@ -8,18 +9,33 @@ function replaceAll(raw, regex, replace) {
   }
   return raw;
 }
-fs.readdir(dir, (err, files) => {
-  if (err) {
-    return;
-  }
-  const requireItems = files
-    .filter((x) => !x.includes('@') && x.includes('.png'))
-    .map((file) => {
-      const name = replaceAll(file, '-', '_').replace('.png', '').toUpperCase();
-      return `export const ${name} = require('./${file}')`;
-    });
-  const outputName = `${dir}/index.ts`;
-  fs.writeFile(outputName, requireItems.join(';\n').concat(';\n'), (wError) => {
-    console.log(wError);
+
+function gen(folder) {
+  const dir = `${dirname}/${folder}`;
+
+  fs.readdir(dir, (err, files) => {
+    if (err) {
+      return;
+    }
+    const requireItems = files
+      .filter(
+        (x) => !x.includes('@') && (x.includes('.png') || x.includes('.jpg')),
+      )
+      .map((file) => {
+        const name = replaceAll(file, '-', '_')
+          .replace('.png', '')
+          .replace('.jpg', '')
+          .toUpperCase();
+        return `export const ${name} = require('./${file}')`;
+      });
+    const outputName = `${dir}/index.ts`;
+    fs.writeFile(
+      outputName,
+      requireItems.join(';\n').concat(';\n'),
+      (wError) => {
+        console.log(wError);
+      },
+    );
   });
-});
+}
+folders.forEach(gen);
